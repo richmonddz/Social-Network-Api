3206d16f2684f989e3e0aef7b4799c0116555dc1
Richmond Dominguez <richmonddominguez@gmail.com>
2022-08-03 22:19:24 -0700

    started on thoughts controller

---
+++ controllers/T-Controller.js
@@ -0,0 +1,33 @@
+
+const {Thoughts, Users} = require('../models');
+
+
+const thoughtsController = {
+
+    createThoughts({params, body}, res) {
+        Thoughts.create(body)
+        .then(({_id}) => {
+            return Users.findOneAndUpdate({ _id: params.userId}, {$push: {thoughts: _id}}, {new: true});
+        })
+        .then(dbThoughtsData => {
+            if(!dbThoughtsData) {
+                res.status(404).json({message: 'No thoughts with this particular ID!'});
+                return;
+            }
+            res.json(dbThoughtsData)
+        })
+        .catch(err => res.json(err)); 
+    },
+
+
+    getAllThoughts(req,res) {
+        Thoughts.find({})
+        .populate({path: 'reactions', select: '-__v'})
+        .select('-__v')
+        // .sort({_id: -1})
+        .then(dbThoughtsData => res.json(dbThoughtsData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
